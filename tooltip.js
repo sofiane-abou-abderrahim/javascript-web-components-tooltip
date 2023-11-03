9974271a597112058a891efaa4ea589698f78f43
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-03 23:30:38 +0200

    created showTooltip method to render a tooltip

--- tooltip.js
+++ tooltip.js
@@ -6,7 +6,14 @@ class Tooltip extends HTMLElement {
   connectedCallback() {
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     this.appendChild(tooltipIcon);
+  }
+
+  _showTooltip() {
+    const tooltipContainer = document.createElement('div');
+    tooltipContainer.textContent = 'This is the tooltip text!';
+    this.appendChild(tooltipContainer);
   }
 }
 
