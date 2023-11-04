3274b9d4fc89776109d03641cc2dd75ae2aa981d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-04 23:26:23 +0200

    styled our tooltip icon in the shadow dom

--- tooltip.js
+++ tooltip.js
@@ -18,9 +18,16 @@ class Tooltip extends HTMLElement {
         ::slotted(.highlight) {
           border-bottom: 1px dotted red;
         }
+        .icon {
+          background-color: black;
+          color: white;
+          padding: 0.15rem 0.5rem;
+          text-align: center;
+          border-radius: 50%;
+        }
       </style>
       <slot>Some default</slot>
-      <span> (?)</span>
+      <span class="icon">?</span>
     `;
   }
 
