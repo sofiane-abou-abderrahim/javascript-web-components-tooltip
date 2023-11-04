05e6fdea970ef6aa37b7ced14f43c6ae9af4af83
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-04 11:31:23 +0200

    worked with attributes on custom elements

--- tooltip.js
+++ tooltip.js
@@ -2,9 +2,13 @@ class Tooltip extends HTMLElement {
   constructor() {
     super();
     this._tooltipContainer;
+    this._tooltipText = 'Some dummy tooltip text.';
   }
 
   connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
@@ -14,7 +18,7 @@ class Tooltip extends HTMLElement {
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'This is the tooltip text!';
+    this._tooltipContainer.textContent = this._tooltipText;
     this.appendChild(this._tooltipContainer);
   }
 
