a3ed35ec4226ae61d1d92c40dcb67cc1a4cdd211
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-04 23:54:29 +0200

    styled custom element inside a paragraph only with :host-context

--- tooltip.js
+++ tooltip.js
@@ -17,6 +17,10 @@ class Tooltip extends HTMLElement {
           background: #ccc;
         }
 
+        :host-context(p) {
+          font-weight: bold;
+        }
+
         .highlight {
           background-color: red;
         }
