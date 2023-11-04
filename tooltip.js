163e9d871770ed9beabdfd0680839b8d48fab69d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-04 11:48:20 +0200

    styled tooltipContainer with background color, color and position

--- tooltip.js
+++ tooltip.js
@@ -14,11 +14,16 @@ class Tooltip extends HTMLElement {
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10';
     this.appendChild(this._tooltipContainer);
   }
 
