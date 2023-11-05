363a7a192234374d4f65451f53c2bf18fd503685
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-11-05 12:47:03 +0200

    added attributeChangedCallback method + static getter to observe the attributes

--- tooltip.js
+++ tooltip.js
@@ -60,6 +60,14 @@ class Tooltip extends HTMLElement {
     this.style.position = 'relative';
   }
 
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(name, oldValue, newValue);
+  }
+
+  static get observedAttributes() {
+    return ['text'];
+  }
+
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
